68d9b51714ab3d42844591855766f25bbc76a385
Vicennun <vicente.nunez7963@gmail.com>
2025-05-29 08:16:21 -0400

    Readme generado

--- CarritoItem/src/main/java/com/EcoMarketSPA/CarritoItem/EcoMarketSpaCarritoItemApplication.java
+++ CarritoItem/src/main/java/com/EcoMarketSPA/CarritoItem/EcoMarketSpaCarritoItemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcoMarketSpaCarritoApplication {
+public class EcoMarketSpaCarritoItemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcoMarketSpaCarritoApplication.class, args);
+		SpringApplication.run(EcoMarketSpaCarritoItemApplication.class, args);
 	}
 
 }
